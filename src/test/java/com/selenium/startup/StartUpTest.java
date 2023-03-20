974374a188704e5842f7ed916df6090f7e6bc0e1
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-20 23:49:56 +0300

    Configuration comment just remove and TestSuite.xml file name added

--- src/test/java/com/selenium/startup/StartUpTest.java
+++ src/test/java/com/selenium/startup/StartUpTest.java
@@ -1,4 +1,12 @@
 package com.selenium.startup;
 
+import org.testng.annotations.Test;
+
 public class StartUpTest {
+
+    @Test
+    public void startUpTest(){
+        System.out.println("StartUp Tested");
+    }
+
 }
